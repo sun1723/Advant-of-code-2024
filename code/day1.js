37cd8242580c188d43d2039ec028b4251a5c914d
peiting <sunpeiting07@gmail.com>
2024-12-30 10:41:58 +1030

    day1 part2

--- code/day1.js
+++ code/day1.js
@@ -2,6 +2,7 @@ import { secureHeapUsed } from "crypto";
 import { readFile } from "../utils/readFile.js";
 
 const splitter = '   ';
+const checkedList=[];
 
 const ascendingOrder = (a,b) => a-b;
 
@@ -23,8 +24,28 @@ const getInputs = ()=>{
 }
 
 const blackBox = ({firstColumn,secondColumn}) => {
-    return firstColumn.reduce((acc,cur,index)=>acc + Math.abs(cur - secondColumn[index])
-    ,0) 
+    // part 1
+    // return firstColumn.reduce((acc,cur,index)=>acc + Math.abs(cur - secondColumn[index])
+    // ,0) 
+    //part 2
+
+    return firstColumn.reduce((acc,cur)=>{
+        // check if it is in the check list
+        if(isCheked(cur)){
+            return acc;
+        }
+        const frequency = getFrequency(secondColumn,cur);
+        checkedList.push(cur);
+        return acc += cur * frequency;
+    },0)
+}
+
+const getFrequency = (arr, num) => {
+    return arr.filter(item =>item === num).length;
+};
+
+const isCheked = (num) => {
+    return checkedList.indexOf(num) >=0;
 }
 
 const getResult = () => {
